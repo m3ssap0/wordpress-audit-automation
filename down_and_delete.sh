7cc23d5a8f68a744086d2f755b61270415e96e4f
Antonio Francesco `m3ssap0` Sardella <antoniofrancesco.sardella@gmail.com>
2024-10-03 10:32:13 +0200

    Added logic to spin up database only after a scan.

--- down_and_delete.sh
+++ down_and_delete.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export SEMGREP_CONFIG=
+echo "*** This will permanently delete the database! ***"
 docker compose down && \
     docker image rm wordpress-audit-automation-audit wordpress-audit-automation-database && \
     docker volume rm wordpress-audit-automation_database
--- up.sh
+++ up.sh
@@ -3,11 +3,11 @@
 if [ -z "$1" ]; then
     echo "Default Semgrep configuration."
     docker compose up -d && \ 
-        sudo docker ps -a
+        docker ps -a
 else
     export SEMGREP_CONFIG=$1
     echo "Custom Semgrep configuration: '$1'."
     docker compose -f docker-compose.yaml -f docker-compose.custom-semgrep-rules.yaml up -d && \
-        sudo docker ps -a
+        docker ps -a
     unset SEMGREP_CONFIG
 fi
